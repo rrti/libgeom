a715b5ea545feaea500053693d83d4c05e34b06d
rrti <none@none>
2016-07-03 16:20:11 +0200

    rename M_* #defines

--- math_defs.hpp
+++ math_defs.hpp
@@ -6,12 +6,12 @@
 #define LIBGEOM_TUPLE_SIZE  (4)
 #define LIBGEOM_MATRIX_SIZE (4 * 4)
 
-#define M_EPS_F 1e-4f
-#define M_EPS_D 1e-4
-#define M_INF_F std::numeric_limits<float>::infinity()
-#define M_NAN_F std::numeric_limits<float>::signaling_NaN()
-#define M_INF_D std::numeric_limits<double>::infinity()
-#define M_NAN_D std::numeric_limits<double>::signaling_NaN()
+#define M_FEPS 1e-4f
+#define M_DEPS 1e-4
+#define M_FINF std::numeric_limits< float>::infinity()
+#define M_DINF std::numeric_limits<double>::infinity()
+#define M_FNAN std::numeric_limits< float>::signaling_NaN()
+#define M_DNAN std::numeric_limits<double>::signaling_NaN()
 
 // slack-space allowed between (coplanar) triangle surfaces
 #define MAX_TRIANGLE_SLACK_SPACE 0.05f
--- tuple.cpp
+++ tuple.cpp
@@ -3,13 +3,13 @@
 #include "./tuple.hpp"
 
 namespace lib_math {
-	static const t_tuple4f zero_tuple_4f = t_tuple4f( 0.0f,  0.0f,  0.0f,  0.0f);
-	static const t_tuple4f ones_tuple_4f = t_tuple4f( 1.0f,  1.0f,  1.0f,  1.0f);
-	static const t_tuple4f  eps_tuple_4f = t_tuple4f(1e-2f, 1e-2f, 1e-2f, 1e-2f);
+	static const t_tuple4f zero_tuple_4f = t_tuple4f(  0.0f,   0.0f,   0.0f,   0.0f);
+	static const t_tuple4f ones_tuple_4f = t_tuple4f(  1.0f,   1.0f,   1.0f,   1.0f);
+	static const t_tuple4f  eps_tuple_4f = t_tuple4f(M_FEPS, M_FEPS, M_FEPS, M_FEPS);
 
-	static const t_tuple4d zero_tuple_4d = t_tuple4d( 0.0,  0.0,  0.0,  0.0);
-	static const t_tuple4d ones_tuple_4d = t_tuple4d( 1.0,  1.0,  1.0,  1.0);
-	static const t_tuple4d  eps_tuple_4d = t_tuple4d(1e-2, 1e-2, 1e-2, 1e-2);
+	static const t_tuple4d zero_tuple_4d = t_tuple4d(   0.0,    0.0,    0.0,    0.0);
+	static const t_tuple4d ones_tuple_4d = t_tuple4d(   1.0,    1.0,    1.0,    1.0);
+	static const t_tuple4d  eps_tuple_4d = t_tuple4d(M_DEPS, M_DEPS, M_DEPS, M_DEPS);
 
 	static const t_tuple4ui zero_tuple_4ui = t_tuple4ui(0,  0,  0,  0);
 	static const t_tuple4ui ones_tuple_4ui = t_tuple4ui(1,  1,  1,  1);
