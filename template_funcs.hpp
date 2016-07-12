6d287afafd427548a631faf477d4d174f75d5334
rrti <none@none>
2016-07-12 23:48:05 +0200

    add vec_slerp to template_funcs

--- template_funcs.hpp
+++ template_funcs.hpp
@@ -2,12 +2,16 @@
 #define LIBGEOM_TEMPLATE_FUNCS_HDR
 
 #include <xmmintrin.h>
+
+#include <algorithm>
 #include <cmath>
-#include <vector>
 
 namespace lib_math {
 	static const size_t POWERS_OF_TWO[] = {1,  2,   4,    8,    16,     32,      64,      128};
 	static const size_t POWERS_OF_TEN[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000};
+
+	template<typename type> struct t_matrix;
+	template<typename type> struct t_vector;
 
 	template<typename type> type  __attribute__ ((force_align_arg_pointer))  sqrt_sse(type v) {
 		__m128 vec = _mm_set_ss(v); vec = _mm_sqrt_ss(vec); return (_mm_cvtss_f32(vec));
@@ -27,6 +31,52 @@ namespace lib_math {
 	template<typename type> type norm(type v, type vmin, type vmax) { return ((v - vmin) / (vmax - vmin)); }
 	template<typename type> type clamp(type v, type vmin, type vmax) { return (std::max<type>(vmin, std::min<type>(vmax, v))); }
 	template<typename type> type square(type v) { return (v * v); }
+
+
+	template<typename type> t_vector<type> vec_slerp_aux(
+		const t_vector<type>& v,
+		const t_vector<type>& w,
+		const type alpha,
+		const type epsilon
+	) {
+		const type cos_angle = lib_math::clamp(v.inner_product(w), type(-1), type(1));
+		const type angle_max = std::acos(cos_angle); // radians
+		const type angle_int = alpha * angle_max;
+
+		// if dot(source, target) ==  1 no interpolation is required
+		// if dot(source, target) == -1 no interpolation is possible
+		if (cos_angle <= (type(-1) + epsilon))
+			return t_vector<type>::zero_vector();
+		if (cos_angle >= (type(1) - epsilon))
+			return v;
+
+		// rotate in world-aligned space around y, let caller transform back
+		return (v.rotate_y_ext(angle_int));
+	}
+
+	template<typename type> t_vector<type> vec_slerp(const t_vector<type>& vz, const t_vector<type>& vw, type alpha) {
+		// Nth-order polynomial vector interpolation over angles
+		//
+		// two (non-colinear) vectors v and w uniquely span a plane
+		// with normal (v cross w); we could rotate directly in this
+		// plane to do interpolation but opt for the simple approach
+		//
+		// construct an axis-system around the normal and make this
+		// align with the world's axis-system simply by transposing
+		// it, such that rotating v around plane-normal to w equals
+		// rotating v' around world-y to w'
+		const t_vector<type> vy = (vz.outer_product(vw)).normalize();
+		const t_vector<type> vx = (vz.outer_product(vy)).normalize();
+
+		// compose axis-system; vy is plane normal
+		const t_matrix<type> m1 = t_matrix<type>(vx, vy, vz);
+		const t_matrix<type> m2 = m1.transpose_rotation();
+
+		// inv-transform source and target to axis-aligned vectors
+		// interpolate in the local space, transform back to world
+		return (m1 * vec_slerp_aux(m2 * vz, m2 * vw, alpha, 0.01f));
+	}
+
 
 	// clamp an angle in radians to the range [0, 2PI]
 	template<typename type> type clamp_angle_rad(type raw_angle) {
@@ -78,19 +128,6 @@ namespace lib_math {
 		}
 
 		return (std::floor(v + type(0.5f)));
-	}
-
-
-	template<typename type> type vec_sum(const std::vector<type>& v) {
-		type r = type();
-		for (size_t n = 0; n < v.size(); n++) {
-			r += v[n];
-		}
-		return r;
-	}
-
-	template<typename type> type vec_avg(const std::vector<type>& v) {
-		return (vec_sum<type>(v) / std::max(1lu, v.size()));
 	}
 };
 
